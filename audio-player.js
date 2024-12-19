1a69ec10748ed796969df0ea624019f6bc4731f7
YARA <122050843+Yara0202@users.noreply.github.com>
2024-12-19 12:32:25 +0700

    Update audio-player.js

--- audio-player.js
+++ audio-player.js
@@ -1,27 +1,25 @@
 let audio;
 
-document.getElementById('startButton').addEventListener('click', () => {
-    if (!audio) {
-        audio = new Audio('./sempurna.mp3');
-        audio.loop = true;
+// Fungsi untuk memutar audio
+export function playAudio() {
+  if (!audio) {
+    audio = new Audio('./assets/sempurna.mp3');
+    audio.loop = true;
 
-        // Ambil waktu terakhir dari sessionStorage
-        const savedTime = sessionStorage.getItem('audioTime');
-        if (savedTime) {
-            audio.currentTime = parseFloat(savedTime);
-        }
+    const savedTime = sessionStorage.getItem('audioTime');
+    if (savedTime) {
+      audio.currentTime = parseFloat(savedTime);
     }
-    audio.play().catch(error => {
-        console.error('Gagal memutar audio:', error);
-    });
+  }
 
-    // Sembunyikan tombol setelah audio diputar
-    document.getElementById('startButton').style.display = 'none';
-});
+  audio.play().catch(error => {
+    console.error('Gagal memutar audio:', error);
+  });
+}
 
-// Simpan waktu audio saat halaman ditutup
-window.addEventListener('beforeunload', () => {
-    if (audio) {
-        sessionStorage.setItem('audioTime', audio.currentTime);
-    }
-});
+// Fungsi untuk menyimpan posisi audio
+export function saveAudioPosition() {
+  if (audio) {
+    sessionStorage.setItem('audioTime', audio.currentTime);
+  }
+}
